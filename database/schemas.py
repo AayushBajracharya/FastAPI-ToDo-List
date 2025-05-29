c8353397af4a427cda10a2407d71ff6024383a3a
Aayush Bajracharya <aayushbajracharya90@gmail.com>
2025-05-29 13:20:39 +0545

    refactor(models and schemas): make id non-nullable, convert updated_at and creation to str and branch name is renamed Refs: #2

--- database/schemas.py
+++ database/schemas.py
@@ -1,12 +1,16 @@
-from .models import Todo
+from typing import List
+from .models import Todo, TodoRead
 
-def individual_data(todo: Todo):
-    return {
-        "id": todo.id,
-        "title": todo.title,
-        "description": todo.description,
-        "status": todo.is_completed,
-    }
+def individual_data(todo: Todo) -> TodoRead:
+    return TodoRead(
+        id=todo.id,
+        title=todo.title,
+        description=todo.description,
+        is_completed=todo.is_completed,
+        is_deleted=todo.is_deleted,
+        updated_at=todo.updated_at,
+        creation=todo.creation
+    )
 
-def all_tasks(todos: list[Todo]):
+def all_tasks(todos: List[Todo]) -> List[TodoRead]:
     return [individual_data(todo) for todo in todos]
